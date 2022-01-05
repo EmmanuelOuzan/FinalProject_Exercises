329ce1a70088a3d60610fb46a1783d2fb882a9c4
EmmanuelOuzan <Emmanuel.ouzan@gmail.com>
2022-01-05 21:55:16 +0200

    Closed secuirty hole

--- DL/db.js
+++ DL/db.js
@@ -1,19 +1,4 @@
 require('dotenv').config({ path: '/.env' })
-// const mongoose = require('mongoose')
-
-// const
-//     userName = 'Admin',
-//     password = 'Admin',
-//     connectionString = `mongodb+srv://${userName}:${password}@emmanuelmongocluster.a8zm2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-
-// console.log('connecting ..')
-// mongoose.connect(connectionString)
-//     .then(() => {
-//         console.log('Mongo connected!')
-//     })
-
-
-    const mongoose = require('mongoose');
-    mongoose.connect(process.env.CONNECTION_STRING)
-        .then(() => console.log('Mongo Connected'))
-    
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTION_STRING)
+    .then(() => console.log('Mongo Connected'))
